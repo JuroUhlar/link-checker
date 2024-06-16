79a4bebf8b8b03e8e130cb3aac503be8779b9be4
Juraj Uhlar <juro.uhlar@gmail.com>
2024-06-16 22:04:16 +0100

    try less conflicts

--- checkSitemap.ts
+++ checkSitemap.ts
@@ -24,37 +24,41 @@ const checkSitemap = async (url: string) => {
   const checkedLinks = new Map<string, HrefSummary>();
 
   const pagesToCheck = await getPagesFromSitemap(url);
-  await Promise.all(
-    pagesToCheck.map(async (page) => {
-      const linksToCheck = await getPageLinks(page);
-      console.log(page);
-      await Promise.all(
-        linksToCheck.map(async ({ href, text }) => {
-          if (checkedLinks.has(href)) {
-            // Already checked, just add the backlink
-            const { result, foundIn } = checkedLinks.get(href)!;
-            checkedLinks.set(href, {
-              result,
-              foundIn: [...foundIn, { page, linkText: text }],
-            });
-          } else {
-            // New link, check it
-            const result = await checkLink(href);
-            console.log(
-              "       ",
-              href,
-              result.ok ? "✅" : "❌",
-              !result.ok ? result.error : ""
-            );
-            checkedLinks.set(href, {
-              result,
-              foundIn: [{ page, linkText: text }],
-            });
-          }
-        })
-      );
-    })
-  );
+  console.log("Pages to check (from sitemap): ", pagesToCheck.length);
+  for (const [index, page] of pagesToCheck.entries()) {
+    console.log(
+      `Checking page ${page}, ${Math.round(
+        ((index + 1) / pagesToCheck.length) * 100
+      )}%`
+    );
+    const linksToCheck = await getPageLinks(page);
+
+    await Promise.all(
+      linksToCheck.map(async ({ href, text }) => {
+        if (checkedLinks.has(href)) {
+          // Already checked, just add the backlink
+          const { result, foundIn } = checkedLinks.get(href)!;
+          checkedLinks.set(href, {
+            result,
+            foundIn: [...foundIn, { page, linkText: text }],
+          });
+        } else {
+          // New link, check it
+          const result = await checkLink(href);
+          // console.log(
+          //   "       ",
+          //   href,
+          //   result.ok ? "✅" : "❌",
+          //   !result.ok ? result.error : ""
+          // );
+          checkedLinks.set(href, {
+            result,
+            foundIn: [{ page, linkText: text }],
+          });
+        }
+      })
+    );
+  }
 
   const brokenLinks = [...checkedLinks.entries()]
     .filter(([_link, { result }]) => !result.ok)
@@ -78,7 +82,7 @@ const checkSitemap = async (url: string) => {
 
   // Write result to file
   writeFileSync(`./result.json`, JSON.stringify(result, null, 2));
-  console.log(JSON.stringify(result, null, 2));
+  // console.log(JSON.stringify(result, null, 2));
   console.log(
     `Checked ${checkedLinks.size} links in ${
       (performance.now() - startTime) / 1000
@@ -88,10 +92,10 @@ const checkSitemap = async (url: string) => {
 };
 
 (async () => {
-  // await checkSitemap("https://dev.fingerprint.com/sitemap.xml");
-  console.log(
-    await checkLink(
-      "https://remix.run/docs/en/v1/guides/constraints#browser-only-code-on-the-server"
-    )
-  );
+  await checkSitemap("https://dev.fingerprint.com/sitemap.xml");
+  // console.log(
+  //   await checkLink(
+  //     "https://remix.run/docs/en/v1/guides/constraints#browser-only-code-on-the-server"
+  //   )
+  // );
 })();
